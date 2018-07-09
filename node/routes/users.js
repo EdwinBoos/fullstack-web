ee9d83cb1c1e107d5710dfd459eacab5f46c18f3
EdwinBoos <edwin.boos@gmx.de>
2018-07-09 20:49:28 +0200

    Return static user array

--- node/routes/users.js
+++ node/routes/users.js
@@ -2,6 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
-router.get('/', (req, res, next) => res.send('respond with a xd'));
+
+router.get('/', (req, res, next) => 
+	res.send( 
+	  [ 
+		{ userId : 1, name : "Foo"}, 
+		{ userId : 2, name : "Foo"},
+		{ userId : 3, name : "admin" } 
+	  ] 
+	));
 
 module.exports = router;
