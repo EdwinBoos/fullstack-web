055a79d5bb535ada64cb1f99f9040f0ec434788f
INFLUENZ <edwin.boos@gmx.de>
2018-07-10 08:40:31 +0200

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,12 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views')); 
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
@@ -18,12 +16,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use((req, res, next) => next(createError(404)) );
+app.use((req, res, next) => next(createError(404)) ); // catch 404 and forward to error handler
 
 // error handler
 app.use((err, req, res, next) => 
