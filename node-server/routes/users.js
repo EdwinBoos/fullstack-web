15d05c3bf67ce67d5e476ac6750c3a841fad341c
e.boos <edwin.boos@gmx.de>
2018-08-20 20:46:54 +0200

    Implement missing CRUD operations

--- node-server/routes/users.js
+++ node-server/routes/users.js
@@ -1,20 +1,41 @@
 const express = require('express');
-const router = express.Router(); 
-const models  = require('../models');
+const router = express.Router();
+const models = require('../models');
 
-router.get('/', (req, res, next) => 
-	models.users.findAll().then((users) => 
-		res.send(users)));
+router.get('/', (req, res, next) =>
+ models.users.findAll().then((users) =>
+  res.send(users)));
 
-router.post('/', (req, res, next) => 
-	models.users.create({ 
-		username: req.body.username, 
-		firstname: req.body.firstname, 
-		lastname: req.body.lastname 
-	}));
+router.get('/:id', (req, res) =>
+ models.users.findById(req.params.id).then((user) =>
+  res.send(user)));
 
-router.put('/', (req, res, next) => res.send('Update successful'));
-router.delete('/', (req, res, next) => res.send('Delete successful'));
+router.post('/', (req, res, next) =>
+
+ models.users.create({
+
+  username: req.body.username,
+  firstname: req.body.firstname,
+  lastname: req.body.lastname
+ }));
+
+router.delete('/:id', (req, res) =>
+ models.users.destroy({
+  where: {
+   id: req.params.id
+  }
+ }).then((user) => res.send(user)));
+
+router.put('/:id', (req, res) =>
+ models.users.update({
+  username: req.body.username,
+  firstname: req.body.firstname,
+  lastname: req.body.lastname
+ }, {
+  where: {
+   id: req.params.id
+  }
+ }).then((user) => res.send(user)));
 
 
 module.exports = router;
