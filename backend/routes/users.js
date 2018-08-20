540e7d5db34b69f37b7700f98c761774bd5461bf
e.boos <edwin.boos@gmx.de>
2018-08-20 17:09:24 +0200

    Implement Users Route Create and Read all operation

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -3,15 +3,21 @@ const router = express.Router();
 const models  = require('../models');
 
 
+
 router.get('/', (req, res, next) => 
-	res.send( 
-	  [ 
-		{ UserId : 1, name : "foo"}, 
-		{ UserId : 2, name : "Foo"},
-		{ UserId : 3, name : "admin" }	 
-	  ] 
-	));
-router.post('/', (req, res, next) => res.send('Post successful'));
+	models.users.findAll().then((users) => 
+	res.send(users);
+));
+
+router.post('/', (req, res, next) => 
+	models.users.create(
+			{ 	
+				username: req.body.username, 
+				firstname: req.body.firstname, 
+				lastname: req.body.lastname 
+			}));
+
+
 router.put('/', (req, res, next) => res.send('Update successful'));
 router.delete('/', (req, res, next) => res.send('Delete successful'));
 
