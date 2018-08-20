fccd874f3b0b203ab93b0abd56e446f289200161
e.boos <edwin.boos@gmx.de>
2018-08-20 16:10:27 +0200

    code

--- backend/models/users.js
+++ backend/models/users.js
@@ -3,10 +3,10 @@ module.exports = (sequelize, DataTypes) =>
 
   const Users = sequelize.define('users', 
     {
-	   	id: { autoIncrement: true, primaryKey: true, type: DataTypes.INTEGER },
-	   	firstname: { type: DataTypes.STRING, notEmpty: true },
-		lastname: { type: DataTypes.STRING, notEmpty: true },
-		username: { type: DataTypes.TEXT, notEmpty: true },
+	  id: { autoIncrement: true, primaryKey: true, type: DataTypes.INTEGER },
+	  firstname: { type: DataTypes.STRING, notEmpty: true },
+	  lastname: { type: DataTypes.STRING, notEmpty: true },
+	  username: { type: DataTypes.TEXT, notEmpty: true },
   	}
   );
 
