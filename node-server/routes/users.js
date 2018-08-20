d769e54223ccecd10b34b10c8590c3c52539df39
e.boos <edwin.boos@gmx.de>
2018-08-20 22:55:43 +0200

    Implement Users route sorting and order by

--- node-server/routes/users.js
+++ node-server/routes/users.js
@@ -1,19 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const models = require('../models');
+const sort =  require('fast-sort');
 
-router.get('/', (req, res, next) =>
- models.users.findAll().then((users) =>
-  res.send(users)));
+router.get('/', (req, res, next) => 
+ models.users.findAll().then((users) => {
+ 	if(req.query.sort) 
+ 	{
+ 		const sortObject = { }
+ 		const order = req.query.order || "asc";
+ 		sortObject[order] = req.query.sort;
+		users = sort(users).by([sortObject]);
+ 	}
+    res.send(users);
+  }
+ ));
 
 router.get('/:id', (req, res) =>
  models.users.findById(req.params.id).then((user) =>
   res.send(user)));
 
 router.post('/', (req, res, next) =>
-
  models.users.create({
-
   username: req.body.username,
   firstname: req.body.firstname,
   lastname: req.body.lastname
