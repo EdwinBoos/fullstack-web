0b12499f7502f5c6464dc0b31520020523073ed8
e.boos <edwin.boos@gmx.de>
2018-08-20 17:16:52 +0200

    Format code

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,22 +2,16 @@ const express = require('express');
 const router = express.Router(); 
 const models  = require('../models');
 
-
-
 router.get('/', (req, res, next) => 
 	models.users.findAll().then((users) => 
-	res.send(users)
-));
+		res.send(users)));
 
 router.post('/', (req, res, next) => 
-	
-	models.users.create(
-			{ 	
-				username: req.body.username, 
-				firstname: req.body.firstname, 
-				lastname: req.body.lastname 
-			}));
-
+	models.users.create({ 
+		username: req.body.username, 
+		firstname: req.body.firstname, 
+		lastname: req.body.lastname 
+	}));
 
 router.put('/', (req, res, next) => res.send('Update successful'));
 router.delete('/', (req, res, next) => res.send('Delete successful'));
