469d4815db7047da990d2254c2875db310f731f9
e.boos <edwin.boos@gmx.de>
2018-08-21 08:12:17 +0200

    Code beautify

--- node-server/models/users.js
+++ node-server/models/users.js
@@ -1,15 +1,25 @@
-module.exports = (sequelize, DataTypes) =>
-{
+module.exports = (sequelize, DataTypes) => {
 
-  const Users = sequelize.define('users', 
-    {
-	  id: { autoIncrement: true, primaryKey: true, type: DataTypes.INTEGER },
-	  firstname: { type: DataTypes.STRING, notEmpty: true },
-	  lastname: { type: DataTypes.STRING, notEmpty: true },
-	  username: { type: DataTypes.TEXT, notEmpty: true },
-  	}
-  );
+ const Users = sequelize.define('users', {
+  id: {
+   autoIncrement: true,
+   primaryKey: true,
+   type: DataTypes.INTEGER
+  },
+  firstname: {
+   type: DataTypes.STRING,
+   notEmpty: true
+  },
+  lastname: {
+   type: DataTypes.STRING,
+   notEmpty: true
+  },
+  username: {
+   type: DataTypes.TEXT,
+   notEmpty: true
+  },
+ });
 
-  return Users;
+ return Users;
 
 }
