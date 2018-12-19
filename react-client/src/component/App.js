dec3b638cb0a93579c7295224fdc2d017f27fbdc
e.boos <edwin.boos@gmx.de>
2018-12-19 13:50:33 +0100

    Add axios and material-ui and use them

--- react-client/src/component/App.js
+++ react-client/src/component/App.js
@@ -1,22 +1,47 @@
 import React, { Component } from 'react';
 import '../css/App.css';
+import axios from 'axios';
+import Card from '@material-ui/core/Card'
+import Button from '@material-ui/core/Button'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
+const styles = {
+  root : { flexGrow : 1 }
+}
 class App extends Component {
   state = {users: []}
 
   componentDidMount() {
-    fetch('/users?sort=id&order=desc')
-      .then(res => res.json())
-      .then(users => this.setState({ users }));
+    axios.get('/users?sort=id&order=desc')
+         .then(users => this.setState({ users : users.data }));
   }
 
   render() {
     return (
-      <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.id} {user.firstname} {user.lastname} {user.username}</div>
+      <div className="classes.root">
+	<AppBar>  
+         <Toolbar> 
+	   <Typography color="inherit" variant="H6">
+	     Users
+	   </Typography>
+         </Toolbar>
+        </AppBar>
+	<Card>
+	<List>
+	{ 
+           this.state.users.map(user =>
+              <ListItem key={user.id}>{user.id} {user.firstname} {user.lastname} {user.username}</ListItem>
         )}
+	</List>
+        </Card>	
+        <Button variant="contained" color="primary">
+        Add 
+	</Button>
+
       </div>
     );
   }
