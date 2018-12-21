b0b6dfa64b4aeb89c8e1cf0b031bc2e30c1dfab9
e.boos <edwin.boos@gmx.de>
2018-12-21 16:12:05 +0100

    Remove <main>

--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, main, withRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, withRouter, Route, Switch } from "react-router-dom";
 import "./css/index.css";
 import App from "./component/App";
 import User from "./component/User";
@@ -8,12 +8,10 @@ import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <Router>
-    <main>
       <Switch>
         <Route exact path="/" component={withRouter(App)} />
         <Route path="/users/:userId" component={withRouter(User)} />
       </Switch>
-    </main>
   </Router>,
   document.getElementById("root")
 );
