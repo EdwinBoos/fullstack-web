be320e128b321a5499934440dd59a090ca2b1515
e.boos <edwin.boos@gmx.de>
2018-12-21 15:31:36 +0100

    Implement routing between pages

--- react-client/src/component/User.js
+++ react-client/src/component/User.js
@@ -1,19 +1,11 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Card from "@material-ui/core/Card";
-import { Link } from "react-router-dom";
-import CardContent from "@material-ui/core/CardContent";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Fade from "@material-ui/core/Fade";
-import Button from "@material-ui/core/Button";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 class App extends Component {
   state = { users: [], loading: false };
 
@@ -32,7 +24,7 @@ class App extends Component {
         <AppBar position="static">
           <Toolbar>
             <Typography color="inherit" variant="h6">
-              Users
+              User
             </Typography>
             <div>
               <Fade in={this.state.loading}>
@@ -42,23 +34,6 @@ class App extends Component {
           </Toolbar>
         </AppBar>
         <Card>
-          <CardContent>
-            <List component="nav">
-              {this.state.users.map((user, index) => (
-                <ListItem key={index} button component={Link} to={`/users/${user.id}`}>
-                  <ListItemIcon>
-                    <AccountCircleIcon> </AccountCircleIcon>
-                  </ListItemIcon>
-                  <ListItemText>
-                    {user.firstname} {user.lastname} {user.username}
-                  </ListItemText>
-                </ListItem>
-              ))}
-            </List>
-            <Button variant="contained" color="primary">
-              Add
-            </Button>
-          </CardContent>
         </Card>
       </div>
     );
--- react-client/src/index.js
+++ react-client/src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, main, withRouter, Route, Switch } from 'react-router-dom';
 import './css/index.css';
 import App from './component/App';
+import User from './component/User';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
 	  <Router>
-	   <App/> 
+	   <main>
+	   <Switch>
+	     <Route exact path="/" component={withRouter(App)} />
+	     <Route path="/users/:userId" component={withRouter(User)} />
+	     </Switch>
+	   </main>
 	  </Router>,
 	  document.getElementById('root'));
 registerServiceWorker();
