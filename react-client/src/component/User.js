1c0d4c9382846e974b88e11f07b415c861295696
INFLUENZ <edwin.boos@gmx.de>
2019-01-16 12:59:15 +0100

    Update User.js

--- react-client/src/component/User.js
+++ react-client/src/component/User.js
@@ -5,8 +5,9 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardActions from "@material-ui/core/CardActions";
 import IconButton from "@material-ui/core/IconButton";
-import CircularProgress from "@material-ui/core/CircularProgress";
+import LinearProgress from "@material-ui/core/LinearProgress";
 import Fade from "@material-ui/core/Fade";
+import Grid from "@material-ui/core/Grid";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
@@ -47,7 +48,9 @@ class User extends Component {
     const { userId } = this.props.match.params;
     this.setState({ user: { photo: { data: "" } }, loading: true });
     axios
-      .put(`/users/${userId}/detail`, userData, { cancelToken: this.source.token })
+      .put(`/users/${userId}/detail`, userData, {
+        cancelToken: this.source.token
+      })
       .then(user => this.setState({ user: user.data, loading: false }))
       .catch(error => {
         if (!axios.isCancel(error)) {
@@ -83,46 +86,47 @@ class User extends Component {
             <Typography color="inherit" variant="h6">
               {this.state.user.username}
             </Typography>
-            <div>
-              <Fade in={this.state.loading}>
-                <CircularProgress color="secondary" />
-              </Fade>
-            </div>
+            <div />
           </Toolbar>
         </AppBar>
-        <Card style={{ maxWidth: 1200 }}>
-          <CardMedia
-            image={`data:image/jpeg;base64,${btoa(
-              new Uint8Array(this.state.user.photo.data).reduce(
-                (data, byte) => data + String.fromCharCode(byte),
-                ""
-              )
-            )}`}
-            style={{ height: 250, paddingTop: "30%" }}
-          />
-          <CardContent>
-            <Typography variant="inherit">
-              first name: {this.state.user.firstname}
-            </Typography>
-            <Typography variant="inherit">
-              last name: {this.state.user.lastname}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <IconButton component="label">
-              <AddAPhotoIcon />
-              <input
-                accept="image/*"
-                onChange={this.handlePictureSelected}
-                type="file"
-                style={{ display: "none" }}
-              />
-            </IconButton>
-            <IconButton onClick={this.handleEditUserPress}>
-              <EditIcon />
-            </IconButton>
-          </CardActions>
-        </Card>
+        <Fade in={this.state.loading}>
+          <LinearProgress color="secondary" />
+        </Fade>
+        <Grid container justify="center">
+          <Card>
+            <CardMedia
+              image={`data:image/jpeg;base64,${btoa(
+                new Uint8Array(this.state.user.photo.data).reduce(
+                  (data, byte) => data + String.fromCharCode(byte),
+                  ""
+                )
+              )}`}
+              style={{ paddingTop: "56.25%", height: 0, width: 1100 }}
+            />
+            <CardContent>
+              <Typography variant="inherit">
+                first name: {this.state.user.firstname}
+              </Typography>
+              <Typography variant="inherit">
+                last name: {this.state.user.lastname}
+              </Typography>
+            </CardContent>
+            <CardActions style={{ justifyContent: "center" }}>
+              <IconButton component="label">
+                <AddAPhotoIcon />
+                <input
+                  accept="image/*"
+                  onChange={this.handlePictureSelected}
+                  type="file"
+                  style={{ display: "none" }}
+                />
+              </IconButton>
+              <IconButton onClick={this.handleEditUserPress}>
+                <EditIcon />
+              </IconButton>
+            </CardActions>
+          </Card>
+        </Grid>
       </div>
     );
   }
