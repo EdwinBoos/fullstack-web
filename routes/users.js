0a550e258d901178b30a843c2a5efe99e05019b5
INFLUENZ <edwin.boos@gmx.de>
2019-01-17 14:37:07 +0100

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -9,9 +9,8 @@ const upload = multer({
  storage,
  fileFilter: (req, file, callback) =>
   (!path.extname(file.originalname).toLowerCase().match(/\.(jpg|jpeg|png|gif)$/)) ?
-  callback(new Error("Only pictures allowed"), false) :
-  callback(null, true)
-});
+  callback(new multer.MulterError(), false) : callback(null, true)
+}).single("photo");
 
 router.get("/", (req, res, next) => {
  const options = {};
@@ -30,16 +29,22 @@ router.get("/:id", (req, res, next) =>
  ).catch(error => next(error))
 );
 
-router.post("/", upload.single("photo"), (req, res, next) =>
- models.users.create({
-  username: req.body.username,
-  photo: (req.file) ? req.file.buffer : void 0,
-  firstname: req.body.firstname,
-  lastname: req.body.lastname
- }).then(user => res.send(user.dataValues))
- .catch(sequelize.UniqueConstraintError, (error) => res.status(500).send("User name already taken"))
- .catch(error => next(error))
-);
+router.post("/", (req, res, next) => {
+ upload(req, res, (error) => {
+  if (error instanceof multer.MulterError) {
+   res.status(300).send("Only images are allowed");
+  } else {
+   models.users.create({
+     username: req.body.username,
+     photo: (req.file) ? req.file.buffer : void 0,
+     firstname: req.body.firstname,
+     lastname: req.body.lastname
+    }).then(user => res.send(user.dataValues))
+    .catch(sequelize.UniqueConstraintError, (error) => res.status(500).send("User name already taken"))
+    .catch(error => next(error))
+  }
+ });
+});
 
 router.delete("/:id/detail", (req, res, next) =>
  models.users
@@ -70,47 +75,61 @@ router.delete("/:id", (req, res, next) =>
  .catch(error => next(error))
 );
 
-router.put("/:id", upload.single("photo"), (req, res, next) => {
- models.users
-  .update({
-   photo: (req.file) ? req.file.buffer : void 0,
-   username: req.body.username,
-   firstname: req.body.firstname,
-   lastname: req.body.lastname
-  }, {
-   where: {
-    id: req.params.id
-   }
-  })
-  .then(() => {
-   const options = {};
-   if (req.query.sort) {
-    options["order"] = [
-     [req.query.sort, req.query.order]
-    ];
-   }
-   models.users.findAll(options).then(users => res.send(users))
-    .catch(error => next(error))
-  }).catch(error => next(error))
-  .catch(sequelize.UniqueConstraintError, (error) => res.status(500).send("User name already taken"))
+router.put("/:id", (req, res, next) => {
+ upload(req, res, (error) => {
+  if (error instanceof multer.MulterError) {
+   res.status(300).send("Only images are allowed");
+  } else {
+   models.users
+    .update({
+     photo: (req.file) ? req.file.buffer : void 0,
+     username: req.body.username,
+     firstname: req.body.firstname,
+     lastname: req.body.lastname
+    }, {
+     where: {
+      id: req.params.id
+     }
+    })
+    .then(() => {
+     const options = {};
+     if (req.query.sort) {
+      options["order"] = [
+       [req.query.sort, req.query.order]
+      ];
+     }
+     models.users.findAll(options).then(users => res.send(users))
+      .catch(error => next(error))
+    }).catch(error => next(error))
+    .catch(sequelize.UniqueConstraintError, (error) => res.status(500).send("User name already taken"))
+
+  }
+ });
 });
 
-router.put("/:id/detail", upload.single("photo"), (req, res, next) => {
- models.users
-  .update({
-   photo: (req.file) ? req.file.buffer : void 0,
-   username: req.body.username,
-   firstname: req.body.firstname,
-   lastname: req.body.lastname
-  }, {
-   where: {
-    id: req.params.id
-   }
-  })
-  .then(() =>
-   models.users.findByPk(req.params.id).then(user => res.send(user))
-  ).catch(error => next(error))
-  .catch(sequelize.UniqueConstraintError, (error) => res.status(500).send("User name already taken"))
+router.put("/:id/detail", (req, res, next) => {
+ upload(req, res, (error) => {
+  if (error instanceof multer.MulterError) {
+   res.status(300).send("Only images are allowed");
+  } else {
+   models.users
+    .update({
+     photo: (req.file) ? req.file.buffer : void 0,
+     username: req.body.username,
+     firstname: req.body.firstname,
+     lastname: req.body.lastname
+    }, {
+     where: {
+      id: req.params.id
+     }
+    })
+    .then(() =>
+     models.users.findByPk(req.params.id).then(user => res.send(user))
+    ).catch(error => next(error))
+    .catch(sequelize.UniqueConstraintError, (error) => res.status(500).send("User name already taken"))
+
+  }
+ });
 });
 
 module.exports = router;
