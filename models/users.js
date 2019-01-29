b78138184f7c75bb3323c8bd3aa9d3051dd7f7d9
INFLUENZ <edwin.boos@gmx.de>
2019-01-29 16:09:46 +0100

    Ignore Cases unique constraint

--- models/users.js
+++ models/users.js
@@ -1,33 +1,40 @@
 module.exports = (sequelize, DataTypes) => {
 
  const Users = sequelize.define('users', {
-  id: {
-   autoIncrement: true,
-   notEmpty: true,
-   allowNull : false,
-   primaryKey: true,
-   type: DataTypes.INTEGER
-  },
-  firstname: {
-   type: DataTypes.STRING,
-   notEmpty: true
-  },
-  lastname: {
-   type: DataTypes.STRING,
-   notEmpty: true
-  },
-  username: {
-   type: DataTypes.STRING,
-   notEmpty: true,
-   unique : { args : true, fields : [sequelize.fn('lower', sequelize.col('username'))]} 
-  },
-  photo : { 
-   type: DataTypes.BLOB,
-   defaultValue: {
-    data: ""
+   id: {
+    autoIncrement: true,
+    notEmpty: true,
+    allowNull: false,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+   },
+   firstname: {
+    type: DataTypes.STRING,
+    notEmpty: true
+   },
+   lastname: {
+    type: DataTypes.STRING,
+    notEmpty: true
+   },
+   username: {
+    type: DataTypes.STRING,
+    notEmpty: true
+   },
+   photo: {
+    type: DataTypes.BLOB,
+    defaultValue: {
+     data: ""
+    }
    }
+  }, {
+   indexes: [{
+    unique: true,
+    name: "unique_name",
+    fields: [sequelize.fn("lower", sequelize.col("username"))]
+   }]
   }
- });
+
+ );
 
  return Users;
 
