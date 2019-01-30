b70ecc40fdc14b3c6f69b514441f6806e46330db
e.boos <edwin.boos@gmx.de>
2019-01-30 14:22:31 +0100

    Add axios

--- react-client/react-native/App.js
+++ react-client/react-native/App.js
@@ -1,11 +1,41 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Axios from "axios";
+import { StyleSheet, Text, View } from "react-native";
 
 export default class App extends React.Component {
+  state = { users: [], loading: false };
+
+  constructor() {
+    super();
+    this.source = Axios.CancelToken.source();
+  }
+
+  componentDidMount() {
+    Axios.get("/users?sort=id&order=desc", { cancelToken: this.source.token })
+      .then(users =>
+        this.setState({
+          users: users.data,
+          loading: false
+        })
+      )
+      .catch(error => {
+        if (!Axios.isCancel(error)) {
+          this.setState({
+            users: [],
+            loading: false
+          });
+        }
+      });
+  }
+
+  componentWillUnmount() {
+    this.source.cancel();
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Welcome</Text>
       </View>
     );
   }
@@ -14,8 +44,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
